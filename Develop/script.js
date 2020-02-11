4baa623345d998fc01c4c55ea7ea35aebf6cfa0c
Lemonade Kidd <smejia03@gmail.com>
2020-02-10 18:01:35 -0600

    randomized generator fixed

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePassword);
+
 
 // Write password to the #password input
 function generatePassword() {
   var charLength = prompt("How many characters do you want in your Next-Gen password?");
-    if (charLength < 8 || charLength > 128){
-      alert("Sorry! Password length must be between 8-128 characters.");
+    if (charLength < 8 || charLength > 128 || isNaN(charLength)){
+      alert("Sorry! Password character length must be a number between 8-128.");
     } else {
       var lowerCaseQ = confirm("Want any lowercase letters?");
       var upperCaseQ = confirm("Want any uppercase letters?");
       var numChar = confirm("How about some numbers?");
       var specialChar = confirm("Do you want any special characters?")
     } 
+   
 
-    var possibleChar = []; //create an if based on the selection choices above, these will go into the possibleChar array
+    var possibleCharArray = []; //create an if based on the selection choices above, these will go into the possibleCharArray array
     var lCase = "abcdefghijklmnopqrstuvwxyz".split("");
     var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); //this split separates all my letters like an array
     var nChars = "1234567890".split("");
     var sChars = "!@#$%^&*".split("");
 
-    if (lowerCaseQ === true) {    // create if statements for the rest of your choice vars
-      possibleChar = possibleChar.concat(lCase);
-      console.log(possibleChar);
+   if (lowerCaseQ === true) {    
+      possibleCharArray = possibleCharArray.concat(lCase);
+      console.log(possibleCharArray);
     }
 
-    var pwd = "";
+    if (upperCaseQ === true) {
+      possibleCharArray = possibleCharArray.concat(uCase);
+      console.log(possibleCharArray);
+    }
 
-    for (var i = 1; i <= charLength; i++){  //for loop will contain the math.random based on the possibleChar.length -1
-    pwd = pwd + possibleChar[//random index will go here]
+    if (numChar === true) {
+      possibleCharArray = possibleCharArray.concat(nChars);
+      console.log(possibleCharArray);
+    }
+
+    if (specialChar === true) {
+      possibleCharArray = possibleCharArray.concat(sChars);
+      console.log(possibleCharArray);
+    }
+      
+
+   var pwd = "";
+
+   for (var i = 0; i < charLength; i++) {  
+     var idx = (Math.floor(Math.random() * possibleCharArray.length));
+      pwd = pwd + (possibleCharArray[idx]);
     }
     
+    console.log(pwd);
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = pwd;
+    
+     
+
     
     
-    return pwd;
-  } 
-    
-
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-
- 
-
- 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
+  }
